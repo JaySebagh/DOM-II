9a9e2479f9f1a1b404f4bb2738081f133a1dd237
JaySebagh <jaysebagh@gmail.com>
2019-01-29 11:17:18 -0800

    added click

--- js/index.js
+++ js/index.js
@@ -2,7 +2,12 @@
 
 // hover "Fun Bus" = gray background
 let name = document.querySelector("h1");
-
 name.addEventListener("mouseover", function() {
     document.body.style.backgroundColor = "gray";
 });
+
+// click anywhere on body = lightblue background
+let bodyclick = document.querySelector("body");
+bodyclick.addEventListener("click", e => {
+    document.body.style.backgroundColor = "lightblue";
+})
