0c71f0c4454b38496514d5a62af8ff345c8d5ee1
JaySebagh <jaysebagh@gmail.com>
2019-01-29 11:06:47 -0800

    added hover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+// hover "Fun Bus" = gray background
+let name = document.querySelector("h1");
+
+name.addEventListener("mouseover", function() {
+    document.body.style.backgroundColor = "gray";
+});
